e2cc50a29ab3f51249d8b2d575f205f58a170a02
unknown <529790884@qq.com>
2020-12-13 14:28:38 +0800

    add 141

--- 141. 环形链表.py
+++ 141. 环形链表.py
@@ -4,20 +4,21 @@ class ListNode:
         self.val = x
         self.next = next
 
+
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        fast = head
-        if fast == None or fast.next == None:
+    def hasCycle(self, head):
+        if not head or not head.next:   #节点不存在或下一个节点不存在
             return False
-        fast = fast.next.next
+        
         slow = head
-        while (fast != slow):
-            if fast == None or fast.next == None:
-                return False
-            fast = fast.next.next
-            slow = slow.next
-            if fast == slow:
+        fast = head.next                #起点错开，防止下面循环判断有问题
+        
+        while fast and fast.next:       #快节点和快节点下一个节点不为空
+            if slow == fast:
                 return True
+            slow = slow.next
+            fast = fast.next.next
+        return False
 
 a = ListNode(1, None)
 b = ListNode(1, None)
